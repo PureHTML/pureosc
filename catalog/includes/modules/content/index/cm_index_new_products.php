9d2427dbda68c9638c81049bcb496f8f6c889df0
SimonFormanek <mail@simonformanek.cz>
2024-10-02 06:11:19 +0200

    DirtyHackFIXME

--- catalog/includes/modules/content/index/cm_index_new_products.php
+++ catalog/includes/modules/content/index/cm_index_new_products.php
@@ -41,19 +41,19 @@ class cm_index_new_products
     {
         global $oscTemplate, $currencies, $languages_id, $current_category_id;
 
-        if (!isset($current_category_id) || $current_category_id === '0') {
+        if (!isset($current_category_id) || $current_category_id == 0) {
             $new_products_query = tep_db_query("select p.*, pd.*, if(s.status, s.specials_new_products_price, p.products_price) as products_price from products p left join specials s on p.products_id = s.products_id, products_description pd where p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '".(int) $languages_id."' order by p.products_date_added desc limit ".(int) MODULE_CONTENT_INDEX_NEW_PRODUCTS_MAX_DISPLAY_PRODUCTS);
         } else {
             $new_products_query = tep_db_query("select distinct p.*, pd.*, if(s.status, s.specials_new_products_price, p.products_price) as products_price from products p left join specials s on p.products_id = s.products_id, products_description pd, products_to_categories p2c, categories c where p.products_id = p2c.products_id and p2c.categories_id = c.categories_id and c.parent_id = '".(int) $current_category_id."' and p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '".(int) $languages_id."' order by p.products_date_added desc limit ".(int) MODULE_CONTENT_INDEX_NEW_PRODUCTS_MAX_DISPLAY_PRODUCTS);
         }
 
         if (tep_db_num_rows($new_products_query) > 0) {
-            $new_products_array = [];
+            $new_products_array_x = [];
 
             while ($new_products = tep_db_fetch_array($new_products_query)) {
                 $new_products['products_price'] = $currencies->display_price($new_products['products_price'], tep_get_tax_rate($new_products['products_tax_class_id']));
 
-                $new_products_array[] = $new_products;
+                $new_products_array_x[] = $new_products;
             }
 
             ob_start();
