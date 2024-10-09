95b7806dc347e13733cf1d417d22e8c4fd57ef0b
Vitex <info@vitexsoftware.cz>
2024-10-09 02:32:51 +0200

    sql sorting fixed for webp_banner

--- catalog/includes/modules/content/index/cm_index_webp_banner.php
+++ catalog/includes/modules/content/index/cm_index_webp_banner.php
@@ -68,7 +68,7 @@ EOD.TABLE_BANNERS.<<<'EOD'
 EOD.MODULE_CAROUSEL_ROTATOR_GROUP.<<<'EOD'
 '
                     and status
-                  order by banners_title
+                  order by banners_title 
 EOD.MODULE_CAROUSEL_ROTATOR_BANNER_ORDER.<<<'EOD'
 
                   limit
