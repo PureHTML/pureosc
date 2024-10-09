3ee0c1ed6f5fe0612618f55cd7078df7acdc6041
SimonFormanek <mail@simonformanek.cz>
2024-10-09 08:38:53 +0200

    emptyCatalogForDeveloppers

---
+++ catalog/install/empty_catalog.sql
@@ -0,0 +1,168 @@
+-- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: vanilla
+-- ------------------------------------------------------
+-- Server version	10.11.6-MariaDB-0+deb12u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `products_id` int(11) NOT NULL AUTO_INCREMENT,
+  `products_quantity` int(4) NOT NULL,
+  `products_model` varchar(12) DEFAULT NULL,
+  `products_image` varchar(255) DEFAULT NULL,
+  `products_price` decimal(15,4) NOT NULL,
+  `products_date_added` datetime NOT NULL,
+  `products_last_modified` datetime DEFAULT NULL,
+  `products_date_available` datetime DEFAULT NULL,
+  `products_weight` decimal(5,2) NOT NULL,
+  `products_status` tinyint(1) NOT NULL,
+  `products_tax_class_id` int(11) NOT NULL,
+  `manufacturers_id` int(11) DEFAULT NULL,
+  `products_ordered` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`products_id`),
+  KEY `idx_products_model` (`products_model`),
+  KEY `idx_products_date_added` (`products_date_added`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products_description`
+--
+
+DROP TABLE IF EXISTS `products_description`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products_description` (
+  `products_id` int(11) NOT NULL AUTO_INCREMENT,
+  `language_id` int(11) NOT NULL DEFAULT 1,
+  `products_name` varchar(128) NOT NULL DEFAULT '',
+  `products_description` text DEFAULT NULL,
+  `products_url` varchar(255) DEFAULT NULL,
+  `products_viewed` int(5) DEFAULT 0,
+  PRIMARY KEY (`products_id`,`language_id`),
+  KEY `products_name` (`products_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products_description`
+--
+
+LOCK TABLES `products_description` WRITE;
+/*!40000 ALTER TABLE `products_description` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products_description` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products_to_categories`
+--
+
+DROP TABLE IF EXISTS `products_to_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products_to_categories` (
+  `products_id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  PRIMARY KEY (`products_id`,`categories_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products_to_categories`
+--
+
+LOCK TABLES `products_to_categories` WRITE;
+/*!40000 ALTER TABLE `products_to_categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products_to_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `categories_id` int(11) NOT NULL AUTO_INCREMENT,
+  `categories_image` varchar(255) DEFAULT NULL,
+  `parent_id` int(11) NOT NULL DEFAULT 0,
+  `sort_order` int(3) DEFAULT NULL,
+  `date_added` datetime DEFAULT NULL,
+  `last_modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`categories_id`),
+  KEY `idx_categories_parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories_description`
+--
+
+DROP TABLE IF EXISTS `categories_description`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories_description` (
+  `categories_id` int(11) NOT NULL DEFAULT 0,
+  `language_id` int(11) NOT NULL DEFAULT 1,
+  `categories_name` varchar(255) NOT NULL,
+  `categories_description` text DEFAULT NULL,
+  PRIMARY KEY (`categories_id`,`language_id`),
+  KEY `idx_categories_name` (`categories_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories_description`
+--
+
+LOCK TABLES `categories_description` WRITE;
+/*!40000 ALTER TABLE `categories_description` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories_description` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-09  0:17:26
