4a9c99ecea9e547b4ad54d39008cfefcacaa67fc
SimonFormanek <mail@simonformanek.cz>
2024-10-11 05:56:07 +0200

    fix:mysql->mariadb

--- bin/csscompiler.php
+++ bin/csscompiler.php
@@ -1,37 +1,36 @@
 #!/usr/bin/php 
 <?php
 chdir('../catalog');
-//require('includes/application_top.php');
+
 include('includes/local/configure.php');
 
 // include the list of project filenames
 //require(DIR_WS_INCLUDES.'filenames.php');
-
 // include the database functions
 require('includes/functions/database.php');
 //require('includes/functions/general.php');
-
 // make a connection to the database... now
 tep_db_connect() or die('Unable to connect to database server!');
 
 // set application wide parameters
 $configuration_query = tep_db_query('select configuration_key as cfgKey, configuration_value as cfgValue from configuration');
-while ($configuration       = tep_db_fetch_array($configuration_query)) {
+while ($configuration = tep_db_fetch_array($configuration_query)) {
     define($configuration['cfgKey'], $configuration['cfgValue']);
 }
 
-echo 'configuration loaded id Store name:' . STORE_NAME . "\n";
+echo 'configuration loaded id Store name:' . constant('STORE_NAME') . "\n";
+
 ////
 // PureHTML Css generator
 //nested fiter:
-function css_compile($data)
-{
-    $data =  preg_replace('/}}}}}}/', '}', preg_replace('/}}}}/', '}}', str_replace(': ', ':', str_replace('; }', '}', str_replace('} ', '}', preg_replace('/\s+/', ' ', preg_replace('/\t/', ' ', preg_replace('/\n/', ' ', preg_replace(':/\*.*\*/:', '', $data)))))))), 1);
+function css_compile($data) {
+    $data = preg_replace('/}}}}}}/', '}', preg_replace('/}}}}/', '}}', str_replace(': ', ':', str_replace('; }', '}', str_replace('} ', '}', preg_replace('/\s+/', ' ', preg_replace('/\t/', ' ', preg_replace('/\n/', ' ', preg_replace(':/\*.*\*/:', '', $data)))))))), 1);
     return $data;
 }
-function cssquery($max = 0, $min = 0, $template = '', $tag = '', $status = 1, $inline = 0)
-{
-//nejprve jedeme max
+
+function cssquery($max = 0, $min = 0, $template = '', $tag = '', $status = 1, $inline = 0) {
+//$min_max = query sort order
+    
     if ($max == 0 && $min == 0) {
         $min_max = 'min';
     } elseif ($max == 0 && $min > 0) {
@@ -43,22 +42,22 @@ function cssquery($max = 0, $min = 0, $template = '', $tag = '', $status = 1, $i
     $old = 0;
     $current = 0;
     $counter = 0;
-    $css_query = tep_db_query("SELECT * from " . TABLE_CSS . " WHERE max=" . $max . " AND min=" . $min . " AND template='" . $template . "' AND status=1 AND inline=0 ORDER BY " . $min_max . ", sort_order");
+    $css_query = tep_db_query("SELECT * from css WHERE max=" . $max . " AND min=" . $min . " AND template='" . $template . "' AND status=1 AND inline=0 ORDER BY " . $min_max . ", sort_order");
     while ($css = tep_db_fetch_array($css_query)) {
-    /*
-    //na zacatek
-    if ($counter == 0){
-        $old = $css[$min_max];
-        if ($old > 0) {
-        $data .= '}' . "\n" . '@media (' . $min_max . '-width: ' . $css[$min_max] . 'px){' . "\n"; //}
-        }
-    }*/
+        
+          //na zacatek
+          if ($counter == 0){
+          $old = $css[$min_max];
+          if ($old > 0) {
+          $data .= '}' . "\n" . '@media (' . $min_max . '-width: ' . $css[$min_max] . 'px){' . "\n"; 
+          }
+          }
         $current = $css[$min_max];
         if ($css[$min_max] == $old) {
-            $data .= $css['name'] . '{' . $css['value']  . '}' . "\n";
+            $data .= $css['name'] . '{' . $css['value'] . '}' . "\n";
         } else { //{
-    //      $data .= 'xxx}' . "\n" . '@media (' . $min_max . '-width: ' . $css[$min_max] . 'px){' . $css['name'] . '{' . $css['value'] .'}' . "\n"; //}
-            $data .= "\n" . '@media (' . $min_max . '-width: ' . $css[$min_max] . 'px){' . $css['name'] . '{' . $css['value'] . '}' . "\n"; //}
+            //      $data .= 'xxx}' . "\n" . '@media (' . $min_max . '-width: ' . $css[$min_max] . 'px){' . $css['name'] . '{' . $css['value'] .'}' . "\n"; //}
+            $data .= "\n" . '@media (' . $min_max . '-width: ' . $css[$min_max] . 'px){' . $css['name'] . '{' . $css['value'] . '}' . "\n"; 
         }
         $old = $current;
         $counter++;
@@ -69,12 +68,12 @@ function cssquery($max = 0, $min = 0, $template = '', $tag = '', $status = 1, $i
 
     return $data;
 }
-function cssgenerator()
-{
+
+function cssgenerator() {
 
     $data = '';
-//1. WHERE max > 0 AND min=0 AND template='' AND subtemplate='' AND inline=0 AND status=1
-    $cssmain_query = tep_db_query("SELECT max AS init from " . TABLE_CSS . " WHERE max > 0 AND min=0 AND template='' AND subtemplate='' AND inline=0 AND status=1 ORDER BY max");
+//1. phone only => WHERE max > 0 AND min=0 AND template='' AND subtemplate='' AND inline=0 AND status=1
+    $cssmain_query = tep_db_query("SELECT max AS init from css WHERE max > 0 AND min=0 AND template='' AND subtemplate='' AND inline=0 AND status=1 ORDER BY max");
     while ($cssmain = tep_db_fetch_array($cssmain_query)) {
         $data .= cssquery($cssmain['init']);
     }
@@ -84,10 +83,10 @@ function cssgenerator()
     $data = css_compile($data);
 
 //2. default values
-    $data .= css_compile(cssquery(0, 0, '', '', '', 1, 0));
+    $data .= css_compile(cssquery()); //defaults: $max = 0, $min = 0, $template = '', $tag = '', $status = 1, $inline = 0
 
-//3. WHERE max = 0 AND min > 0 AND template='' AND subtemplate='' AND inline=0 AND status=1
-    $cssmain_query = tep_db_query("SELECT DISTINCT min AS init from " . TABLE_CSS . " WHERE max = 0 AND min > 0 AND template='' AND subtemplate='' AND inline=0 AND status=1 ORDER BY min");
+//3. desktop only => WHERE max = 0 AND min > 0 AND template='' AND subtemplate='' AND inline=0 AND status=1
+    $cssmain_query = tep_db_query("SELECT DISTINCT min AS init from css WHERE max = 0 AND min > 0 AND template='' AND subtemplate='' AND inline=0 AND status=1 ORDER BY min");
     while ($cssmain = tep_db_fetch_array($cssmain_query)) {
         $data .= cssquery(0, $cssmain['init']);
     }
@@ -100,12 +99,11 @@ function cssgenerator()
     file_put_contents(DIR_FS_CATALOG . 's_debug.css', $data, 644);
 }
 
-function cssgenerator_index_top()
-{
+function cssgenerator_index_top() {
 
     $data = '';
 //1. WHERE max > 0 AND min=0 AND template='index_top' AND  inline=0 AND status=1
-    $cssmain_query = tep_db_query("SELECT max AS init from " . TABLE_CSS . " WHERE max > 0 AND min=0 AND template='index_top' AND  inline=0 AND status=1 ORDER BY max");
+    $cssmain_query = tep_db_query("SELECT max AS init from css WHERE max > 0 AND min=0 AND template='index_top' AND  inline=0 AND status=1 ORDER BY max");
     while ($cssmain = tep_db_fetch_array($cssmain_query)) {
         $data .= cssquery($cssmain['init'], 0, 'index_top');
     }
@@ -118,7 +116,7 @@ function cssgenerator_index_top()
     $data .= css_compile(cssquery(0, 0, 'index_top', '', 1, 0));
 
 //3. WHERE max = 0 AND min > 0 AND template='index_top' AND  inline=0 AND status=1
-    $cssmain_query = tep_db_query("SELECT DISTINCT min AS init from " . TABLE_CSS . " WHERE max = 0 AND min > 0 AND template='index_top' AND  inline=0 AND status=1 ORDER BY min");
+    $cssmain_query = tep_db_query("SELECT DISTINCT min AS init from css WHERE max = 0 AND min > 0 AND template='index_top' AND  inline=0 AND status=1 ORDER BY min");
     while ($cssmain = tep_db_fetch_array($cssmain_query)) {
         $data .= cssquery(0, $cssmain['init'], 'index_top');
     }
@@ -130,12 +128,12 @@ function cssgenerator_index_top()
     $data = str_replace("{", "\n{", str_replace("}", "}\n", $data));
     file_put_contents(DIR_FS_CATALOG . 'index_top_debug.css', $data, 644);
 }
-function cssgenerator_index_products()
-{
+
+function cssgenerator_index_products() {
 
     $data = '';
 //1. WHERE max > 0 AND min=0 AND template='index_products' AND  inline=0 AND status=1
-    $cssmain_query = tep_db_query("SELECT max AS init from " . TABLE_CSS . " WHERE max > 0 AND min=0 AND template='index_products' AND  inline=0 AND status=1 ORDER BY max");
+    $cssmain_query = tep_db_query("SELECT max AS init from css WHERE max > 0 AND min=0 AND template='index_products' AND  inline=0 AND status=1 ORDER BY max");
     while ($cssmain = tep_db_fetch_array($cssmain_query)) {
         $data .= cssquery($cssmain['init'], 0, 'index_products');
     }
@@ -148,7 +146,7 @@ function cssgenerator_index_products()
     $data .= css_compile(cssquery(0, 0, 'index_products', '', 1, 0));
 
 //3. WHERE max = 0 AND min > 0 AND template='index_products' AND  inline=0 AND status=1
-    $cssmain_query = tep_db_query("SELECT DISTINCT min AS init from " . TABLE_CSS . " WHERE max = 0 AND min > 0 AND template='index_products' AND  inline=0 AND status=1 ORDER BY min");
+    $cssmain_query = tep_db_query("SELECT DISTINCT min AS init from css WHERE max = 0 AND min > 0 AND template='index_products' AND  inline=0 AND status=1 ORDER BY min");
     while ($cssmain = tep_db_fetch_array($cssmain_query)) {
         $data .= cssquery(0, $cssmain['init'], 'index_products');
     }
@@ -161,3 +159,7 @@ function cssgenerator_index_products()
     file_put_contents(DIR_FS_CATALOG . 'index_products_debug.css', $data, 644);
     echo '$data:' . $data . "\n";
 }
+
+cssgenerator();
+cssgenerator_index_top();
+cssgenerator_index_products();
