3a422822a1db09171e10e66deff140d5c0168a07
SimonFormanek <mail@simonformanek.cz>
2024-10-13 07:02:57 +0200

    QuickSearch form formated WITHOUT table

--- catalog/includes/modules/header/templates/quick_search.php
+++ catalog/includes/modules/header/templates/quick_search.php
@@ -1,5 +1,5 @@
   <?php echo tep_draw_form('quick_find', tep_href_link('advanced_search_result.php', '', $request_type, false), 'get').tep_draw_hidden_field('search_in_description', '1').tep_hide_session_id(); ?>
-<table><tr>
-        <td><?php echo tep_draw_input_field('keywords', '', 'class="form-control" placeholder="'.IMAGE_BUTTON_SEARCH.'"'); ?></td>
-    <button type="submit"></button></td>
-</tr></table>  </form>
+    <?php echo tep_draw_input_field('keywords', '', 'class="form-control" placeholder="'.IMAGE_BUTTON_SEARCH.'"'); ?>
+
+    <button type="submit"></button>
+  </form>
